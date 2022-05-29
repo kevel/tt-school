fcd99015e3640c3cd6b415845e411b37ac2172b3
Pavel Kavalenka <kevel321@gmail.com>
2022-05-29 19:10:12 +0200

    Install Vuetify 3

---
+++ vue/src/plugins/vuetify.ts
@@ -0,0 +1,18 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+import { VRow, VCol, VContainer } from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+    components: {
+        VRow,
+        VCol,
+        VContainer,
+    },
+    // directives,
+})
